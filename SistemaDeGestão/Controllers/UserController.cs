04aba060d94a062a6e22dbe60b5cb6b8a5e16bba
randolfoCarvalho <randolfoirades@gmail.com>
2024-07-10 16:28:41 -0300

    Adiciona criacao de usuario

--- SistemaDeGestão/Controllers/UserController.cs
+++ SistemaDeGestão/Controllers/UserController.cs
@@ -9,17 +9,32 @@ namespace SistemaDeGestão.Controllers
     {
         private readonly DataBaseContext _context;
 
-        /* public UserController(DataBaseContext context)
+        public UserController(DataBaseContext context)
         {
             _context = context;
         }
+        /* public UserController(DataBaseContext context)
+        {
+            _context = context;
+        } */
         public IActionResult list()
         {
            return Ok(_context.Users.ToList());
         }
         public IActionResult criarUser([FromBody] User user)
         {
+            try
+            {
+                _context.Users.Add(user);
+                _context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Erro ao criar novo usuario" + e.Message);
+            }
+            return Ok(user);
         }
+        /*
         public IActionResult Autenticar([FromBody] User user)
         {
            
