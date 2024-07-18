1da0eb1a0cb3926265da200b702eb59d70eb94cb
randolfoCarvalho <randolfoirades@gmail.com>
2024-07-17 23:00:59 -0300

    Retira resize

--- SistemaDeGestão/obj/Debug/net8.0/SistemaDeGestão.AssemblyInfo.cs
+++ SistemaDeGestão/obj/Debug/net8.0/SistemaDeGestão.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaDeGestão")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f15c23713353e3d2d14f02d5e64ca033af8117c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05cb036021cfd240761b3d6c7b945a011fec1d5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaDeGestão")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaDeGestão")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
